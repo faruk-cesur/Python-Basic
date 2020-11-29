3de42fdd945ab572f324ab5ac4ab5c6984b1afa1
faruk-cesur <faruk-cesur@hotmail.com>
2020-11-29 19:18:05 +0300

    29.11.2020 (19:18)

--- Udemy Dersleri/4_loops_problems.py
+++ Udemy Dersleri/4_loops_problems.py
@@ -1,86 +1,91 @@
 # Problem 1
-
 # Kullanıcıdan aldığınız bir sayının mükemmel olup olmadığını bulmaya çalışın.
 # Bir sayının kendi hariç bölenlerinin toplamı kendine eşitse bu sayıya "mükemmel sayı" denir. 
 # Örnek olarak, 6 mükemmel bir sayıdır. (1 + 2 + 3 = 6)
 
-# sayi = int(input('Bir Sayı Giriniz: '))
-
-# toplam = 0
-
-# for i in range(1,sayi):
-#     if sayi % i == 0:
-#         toplam += i
-
-# if sayi == toplam:
-#     print(f'{sayi} MÜKEMMEL SAYIDIR!') 
-# else:
-#     print(f'{sayi} MÜKEMMEL SAYI DEĞİLDİR!')
-
-
-# Problem 2
-
-# Kullanıcıdan aldığınız bir sayının "Armstrong" sayısı olup olmadığını bulmaya çalışın.
-# Örnek olarak, Bir sayı eğer 4 basamaklı ise ve oluşturan rakamlardan herbirinin 
-# 4. kuvvetinin toplamı( 3 basamaklı sayılar için 3.kuvveti ) o sayıya eşitse bu sayıya 
-# "Armstrong" sayısı denir.
-# Örnek olarak : 1634 = 1^4 + 6^4 + 3^4 + 4^4
-
-sayi2 = input('Bir Sayı Giriniz: ')
+sayi = int(input('Bir Sayı Giriniz: '))
 
 toplam = 0
 
-for i in sayi2:
-    toplam += int(i)**len(sayi2)
+for i in range(1,sayi):
+    if sayi % i == 0:
+        toplam += i
 
-if toplam == int(sayi2):
-    print(f'{sayi2} Armstrong Sayısıdır!')
+if sayi == toplam:
+    print(f'{sayi} MÜKEMMEL SAYIDIR!') 
 else:
-    print(f'{sayi2} Armstrong Sayısı Değildir!')
+    print(f'{sayi} MÜKEMMEL SAYI DEĞİLDİR!')
+
+
+# Problem 2
+# Kullanıcıdan aldığınız bir sayının "Armstrong" sayısı olup olmadığını bulmaya çalışın.
+# Örnek olarak, Bir sayı eğer 4 basamaklı ise ve oluşturan rakamlardan herbirinin 
+# 4. kuvvetinin toplamı( 3 basamaklı sayılar için 3.kuvveti ) o sayıya eşitse bu sayıya "Armstrong" sayısı denir.
+# Örnek olarak : 1634 = 1^4 + 6^4 + 3^4 + 4^4
+
+sayi = input('Bir Sayı Giriniz: ')
+
+toplam = 0
+
+for i in sayi:
+    toplam += int(i)**len(sayi)
+
+if toplam == int(sayi):
+    print(f'{sayi} Armstrong Sayısıdır!')
+else:
+    print(f'{sayi} Armstrong Sayısı Değildir!')
 
 
 # Problem 3
 # 1'den 10'kadar olan sayılarla ekrana çarpım tablosu bastırmaya çalışın.
-# İpucu: İç içe 2 tane for döngüsü kullanın. 
-# Aynı zamanda sayıları range() fonksiyonunu kullanarak elde edin.
 
-
-
-
-
-
-
+for i in range(1,11):
+    print('****************')
+    for j in range(1,11):
+        print(f'{i}x{j} = {i*j}')
 
 
 
 # Problem 4
 # Her bir while döngüsünde kullanıcıdan bir sayı alın ve kullanıcının girdiği sayıları 
-# "toplam" isimli bir değişkene ekleyin. Kullanıcı "q" tuşuna bastığı zaman döngüyü sonlandırın 
-# ve ekrana "toplam değişkenini" bastırın.
-# İpucu : while döngüsünü sonsuz koşulla başlatın ve kullanıcı q'ya basarsa döngüyü break ile sonlandırın.
+# "toplam" isimli bir değişkene ekleyin. Kullanıcı "1" tuşuna bastığı zaman döngüyü sonlandırın ve ekrana "toplam değişkenini" bastırın.
 
-
-
-
-
-
-
+print("""********************************
+SAYI GİRME PROGRAMI
+********************************""")
+toplam = 0
+while True:
+    print(' ANA MENÜ '.center(50,'*'))
+    islem = int(input('1. Programdan Çıkış Yap!\n2. Bir Sayı Gir!\n3. Toplamı Görüntüle!\n\nYapılacak İşlem Numarası: '))
+    if islem == 1:
+        print(f'\nToplam Sayı: {toplam}\n\nProgramdan Çıkış Yapılıyor...\n')
+        break
+    elif islem == 2:
+        print(' SAYI GİRME EKRANI '.center(50,'*'))     
+        sayi = int(input('Bir Sayı Giriniz: '))
+        toplam += sayi
+        print(f'\nToplam Sayı: {toplam}\n')
+    elif islem == 3:
+        print(' TOPLAM SAYI EKRANI '.center(50,'*'))
+        print(f'\nToplam Sayı: {toplam}\n')
+    else:
+        print('Geçersiz Giriş Yaptınız. Lütfen 3 Seçenekten Birini Seçiniz!')
 
 
 
 # Problem 5
-# 1'den 100'e kadar olan sayılardan sadece 3'e bölünen sayıları ekrana bastırın. Bu işlemi continue ile yapmaya çalışın.
+# 1'den 100'e kadar olan sayılardan sadece 3'e bölünen sayıları ekrana bastırın.
 
-
-
-
-
+for i in range(1,101):
+    if(i%3 == 0):
+        print("3'e bölünen sayılar: ",i)
+    else:
+        continue
 
 
 # Problem 6
+# list comprehension kullanarak 1'den 100'e kadar olan sayılardan sadece çift sayıları bir listeye atın.
 
-# Buradaki problemin çözümünü derslerimizde özellikle öğrenmedik. Burada mantık yürüterek ve 
-# list comprehension kullanarak 1'den 100'e kadar olan sayılardan sadece çift sayıları bir listeye atmayı yapmayı çalışın.
-# Not: Programlamada her detayı öğrenemeyiz. Bunun için bazı yerlerde deneme yanılma yoluyla da öğrendiğimiz şeyler olur. 
-# Bu problemde deneme yanılma yoluyla list comprehension'ın koşullu durumlarla kullanımını öğreneceksiniz.
-# İpucu: Basit düşünmeye çalışın.
+liste = [i for i in range(1,101) if i % 2 == 0]
+
+print(liste)
